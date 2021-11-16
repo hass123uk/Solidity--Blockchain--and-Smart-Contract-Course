283a73f012720d1376ae4abbf616497b171b818e
Hassan Mahmud <Hassan.Mahmud@lego.com>
2021-11-16 21:15:13 +0100

    Import chainlink data feed & add minUSD to fund action

--- fundme.sol
+++ fundme.sol
@@ -2,12 +2,24 @@
 
 pragma solidity ^0.8.10;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     address payable owner;
     mapping(address => uint256) public addressToAmountMap;
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Rinkby Test net
+     * Aggregator: ETH/USD
+     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     * https://docs.chain.link/docs/ethereum-addresses/
+     */
+     address internal priceFeedAddress = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
 
     constructor() {
         owner = payable(msg.sender);
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
     
     modifier onlyOwner() {
@@ -15,13 +27,25 @@ contract FundMe {
         _;
     }
 
-    function withdraw() payable onlyOwner public {
+    function withdraw() public payable onlyOwner {
         owner.transfer(address(this).balance);
     }
 
-    function deposit() payable public {
-        require(msg.value >= 5, "Value send me be greater then 5 wei");
+    function fund() public payable {
+        uint256 minUSD = 5 * (10 ** 18);
+        require(getConversionRate(msg.value) >= minUSD, "Must send at least 5 USD worth of ETHER");
 
         addressToAmountMap[msg.sender] += msg.value;
+    } 
+    
+    function getConversionRate(uint256 weiAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice() *  10000000000;
+        uint256 ethAmountInUsd = (ethPrice * weiAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+    
+    function getPrice() public view returns (uint256) {
+        (,int price,,,) = priceFeed.latestRoundData();
+        return uint256(price);
     }
 }
