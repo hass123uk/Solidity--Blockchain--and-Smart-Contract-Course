c03b21bcbf01432c0cd29b33b9d9950e3837d7c3
Hassan Mahmud <Hassan.Mahmud@lego.com>
2021-11-16 06:05:14 +0100

    Add withdraw and deposit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Solidity, Blockchain, and Smart Contract Course – Beginner to Expert Python Tutorial
 
 [Course Repo](https://github.com/smartcontractkit/full-blockchain-solidity-course-py)
+
+### Related links:
+
+[Great example of sending-ether](https://solidity-by-example.org/sending-ether/)
+[Re-entrancy hack](https://solidity-by-example.org/hacks/re-entrancy/)
--- fundme.sol
+++ fundme.sol
@@ -1,20 +1,32 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.10;
 
 contract FundMe {
-    address public owner;
+    address payable owner;
+    mapping(address => uint256) public addressToAmountMap;
 
     constructor() {
-        owner = msg.sender;
+        owner = payable(msg.sender);
     }
     
     modifier onlyOwner() {
         require(msg.sender == owner, "You must be the deployer of this contract.");
         _;
     }
-    
-    function withdraw() onlyOwner public view returns (uint256) {
-        return 123;
+
+    function withdraw() payable onlyOwner public {
+        owner.transfer(address(this).balance);
+    }
+
+    function withdrawWithCall() payable onlyOwner public {
+        (bool sent,) = owner.call{value: address(this).balance}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    function deposit() payable public {
+        require(msg.value >= 5, "Value send me be greater then 5 wei");
+
+        addressToAmountMap[msg.sender] += msg.value;
     }
 }
